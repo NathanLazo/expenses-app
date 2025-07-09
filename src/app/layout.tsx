943814010f392630e76d6904f6053434cf9fbef7
Nathan Lazo <84787060+NathanLazo@users.noreply.github.com>
2025-07-09 07:15:54 -0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { BackButton } from "~/components/BackButton";
+import Link from "next/link";
+import { ChartBar, List, Plus, Settings } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Gastos Daniela y Nathan",
@@ -25,9 +27,25 @@ export default function RootLayout({
       <body>
         <TRPCReactProvider>
           <div className="flex min-h-screen flex-col">
-            <header className="border-border flex items-center border-b p-4">
+            <header className="border-border flex items-center gap-4 border-b p-6">
               <BackButton />
               <h1 className="ml-4 text-xl font-semibold">Gastos</h1>
+              <Link href="/reports" className="flex items-center gap-2">
+                <ChartBar className="h-4 w-4" />
+                <p className="hidden md:block">Reportes</p>
+              </Link>
+              <Link href="/settings" className="flex items-center gap-2">
+                <Settings className="h-4 w-4" />
+                <p className="hidden md:block">Configuración</p>
+              </Link>
+              <Link href="/categories" className="flex items-center gap-2">
+                <List className="h-4 w-4" />
+                <p className="hidden md:block">Categorías</p>
+              </Link>
+              <Link href="/expenses" className="flex items-center gap-2">
+                <Plus className="h-4 w-4" />
+                <p className="hidden md:block">Agregar Gasto</p>
+              </Link>
             </header>
             <main className="flex-1 p-6">{children}</main>
             <footer className="border-border text-muted-foreground border-t p-4 text-center text-sm">
